4ec7828d3fde677cf2cc55c09148cd5de12b8c5a
Soumya Ranjan Pradhan <soumya.pradhan5501@gmail.com>
2025-02-06 12:34:10 +0530

    pls do it

---
+++ src/main/java/com/crm/sample/MyDream.java
@@ -0,0 +1,15 @@
+package com.crm.sample;
+
+public class MyDream {
+  public void dream() {
+	  System.out.println("i want to buy one superbike");
+  }
+  public void fathersDream() {
+	  System.out.println("just to settle in a good life having a good job");
+  }
+  public static void main(String[] args) {
+	MyDream md = new MyDream();
+    md.dream();
+    md.fathersDream();
+  }
+}
